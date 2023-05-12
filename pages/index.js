2ad3688d3d7c6d0364c88cf8a4daa44f1d77e659
eun_su_kim <sadbc1235@gmail.com>
2023-05-12 17:35:07 +0900

    #2.7 해야됨

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Seo from "@/components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Home({movies}) {
@@ -12,14 +14,38 @@ export default function Home({movies}) {
     //     })();
     // }, [])
 
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push({
+            pathname: `/movies/${id}`,
+            query: {
+                title
+            }
+        }, `/movies/${id}`);
+    }
+
     return (
         <div className="container">
         <Seo title="Home" />
         {/* {!movies && <h4>Loading...</h4>} */}
         {movies?.map((movie) => (
-            <div className="movie" key={movie.id}>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-            <h4>{movie.original_title}</h4>
+            <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                <h4>
+                    <Link 
+                        href={{
+                            pathname: `/movies/${movie.id}`,
+                            query: {
+                                title: movie.original_title
+                            }
+                        }} 
+                        as={`/movies/${movie.id}`}
+                        legacyBehavior 
+                        key={movie.id}
+                    >
+                        <a> {movie.original_title} </a>
+                    </Link>
+                </h4>
             </div>
         ))}
         <style jsx>{`
